76b429b00bf31eda4447f9229909a9b075e1ddba
ArsalJafri <154714497+ArsalJafri@users.noreply.github.com>
2024-08-11 21:01:11 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+
+import React from "react"
+import Header from "./components/Header.jsx"
+import Meme from "./components/Meme.jsx"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ index.jsx
@@ -0,0 +1,10 @@
+//index.js
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // Updated extension
+import './style.css';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
